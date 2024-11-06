f81620fc030ad62f1781571e93de8d5e38143d24
Gabriel Deganello Freyer <144915279+Degasfr@users.noreply.github.com>
2024-11-05 21:40:10 -0300

    aula 5/11

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 function criarCartão (item, pergunta, resposta) {
-    console.log(item,perrgunta, resposta)
+    //console.log(item,perrgunta, resposta)
+
+    let flashcard = document.getElementById('flashcard');
+    let cartao = document.createElement('article');
+
+    cartao.className = 'cartao';
+
+    cartao.innerHTML = `
+           <div class="cartao__conteudo">
+                <h3>missão</h3>
+                
+                <div class="cartao__conteudo__pergunta">
+                    Saiba qual é a missão de nossa empresa!
+                </div>
+                
+                <div class="cartao__conteudo__pergunta">
+                    Nossa missão é...
+                </div>
+            </div>
+    `
+    flashcard.appendChild(cartao)
+
 }
