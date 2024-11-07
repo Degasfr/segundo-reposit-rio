f5cb212b9f957e630d1375860ebb5311b2c4ad84
Gabriel Deganello Freyer <144915279+Degasfr@users.noreply.github.com>
2024-11-06 22:05:47 -0300

    edição06/11

--- missãoVisãoValores.js
+++ missãoVisãoValores.js
@@ -1,14 +1,15 @@
 criarCartao(
     'Missão',
     'Saiba qual é a missão de nossa empresa!',
-    ' Nossa missão é...'
+    ' Nossa missão é, entregar aos clientes com trufas de qualidade sem igual, feito com dedicação e ingredientes de primeira linha, proporcionando sabor único'
+
 )
 
 
 criarCartao(
     'Visão', 
     'Saiba qual é a visão de nossa empresa!',
-    ' Nossa visão é...'
+    ' Nossa visão é, ter um grande nome no mercado de trufas, prestigiada pela preferencia na trufanello e compromisso com sustentabilidade'
 )
 
 
@@ -16,5 +17,5 @@ criarCartao(
 criarCartao(
     'Valores', 
     'Saiba quais são nossos valores de nossa empresa!',
-    ' Nossos valores são...'
+    ' Nossos valores são, sabor único, excelência em atendimento e qualidade'
 )
