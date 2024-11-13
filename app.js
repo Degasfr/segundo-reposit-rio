95de3dc052f656ef9c122b2baa5c359b7cdac051
Gabriel Deganello Freyer <144915279+Degasfr@users.noreply.github.com>
2024-11-13 20:09:31 -0300

    edição11

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
-function criarCartao (item, pergunta, resposta) {
-    //console.log(item,perrgunta, resposta)
-
-    let flashcard = document.getElementById('flashcard');
-    let cartao = document.createElement('article');
-
-    cartao.className = 'cartao';
+function criaCartao(item, pergunta, resposta) {
+    //console.log(item, pergunta, resposta)
+    let flashcard = document.getElementById('flashcard')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
     cartao.innerHTML = `
-           <div class="cartao__conteudo">
-                <h3>${item}</h3>
-                
-                <div class="cartao__conteudo__pergunta">
-                    <p>${pergunta}</p>
-                </div>
-                
-                <div class="cartao__conteudo__pergunta">
-                    <p>${resposta}</p>
-                </div>
+        <div class="cartao__conteudo">
+            <h3>${item}</h3>
+            
+            <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
             </div>
+            
+            <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+            </div>
+        </div>
     `
+    let respostaEstaVisivel = false
 
-    let respostaEstavisivel = false;
-
-    function viraCartao () {
-        respostaEstavisivel = !variavelEstaVisivel;
-        cartao.classList.toggle('active',viraCartao);
-
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
     }
 
     cartao.addEventListener('click', viraCartao)
